4806adaa0c6541f37ea9896f0fde7ad2480b29c1
Vath12 <69534310+Vath12@users.noreply.github.com>
2022-07-31 07:10:41 -0400

    completed graphics for tictactoe and connect4

--- gameboard.py
+++ gameboard.py
@@ -1,4 +1,7 @@
 import numpy as np,pygame
+
+CONNECT4_STYLE = 0
+TICTACTOE_STYLE = 1
 class GameBoard():
 
     def __init__(self,dimensions,style):
@@ -15,19 +18,57 @@ class GameBoard():
         
         corner = position - size/2
 
-        if (self.style == 0):
+        if (self.style == CONNECT4_STYLE):
             
             TEAMCOLORS = ((255,255,255),(255,0,0),(255,255,0))
-
+            #TEAMCOLORS references the three colors a "node" can have in connect 4
+            #0 being empty or white
+            #1 being team1 represented red
+            #2 being team2 represented by yellow
             pygame.draw.rect(surface,(0,17,50),(corner[0],corner[1],size[0],size[1]))
+            #drawing each cell is a pain because cell dimensions are relative to the boardsize
+            #0.5 is added to x and y because pygame draws circles centered on the coordinates while x and y here map to the corners of each cell
+            #so effectively x = 0 y = 0 is the corner of cell(0,0) and x = 0.5 y = 0.5 is the center of cell(0,0)
             for x in range(len(self.grid)):
                 for y in range(len(self.grid[0])):
                     pygame.draw.circle(surface,TEAMCOLORS[self.grid[x][y]],
                     corner + size*np.array(((x+0.5)/len(self.grid),(y+0.5)/len(self.grid[1]))),
-                    int((size[0]/len(self.grid)) * 0.8*0.5) )
+                    int((size[0]/len(self.grid)) * 0.4) )
+
+        if (self.style == TICTACTOE_STYLE):
+            
+            pygame.draw.rect(surface,(255,255,255),(corner[0],corner[1],size[0],size[1]))
+            
+            for x in range(len(self.grid)):
+                for y in range(len(self.grid[0])):
+                    #drawing the gridCell's bounds
+                    pos = corner + size*np.array(((x)/len(self.grid),(y)/len(self.grid[1])))
+                    bounds = size*np.array(((1)/len(self.grid),(1)/len(self.grid[1])))
+                    pygame.draw.rect(surface,(0,0,0),(pos[0],pos[1],bounds[0],bounds[1]),int(size[0]/len(self.grid[0])*0.05))
+
+                    #team 1 is Xs
+                    if (self.grid[x][y] == 1):
+                        
+                        #the X consists of two lines and it is easy to draw them from the corners of a square
+                        #this square is of size xBounds, centered in the cell
+                        xBounds = np.array(size[0]/len(self.grid[0]),size[1]/len(self.grid[1])) * 0.4
+                        xCenter = corner + (size*np.array(((x+0.5)/len(self.grid),(y+0.5)/len(self.grid[1]))))
+
+                        pygame.draw.line(surface,(255,0,0),xCenter+(xBounds*np.array((-1,-1))),xCenter+(xBounds*np.array((1,1))),int(size[0]/len(self.grid[0])*0.1))
+                        pygame.draw.line(surface,(255,0,0),xCenter+(xBounds*np.array((-1,1))),xCenter+(xBounds*np.array((1,-1))),int(size[0]/len(self.grid[0])*0.1))
+
+                    #team 2 is Os
+                    if (self.grid[x][y] == 2):
+                        pygame.draw.circle(surface,(0,0,255),
+                        corner + size*np.array(((x+0.5)/len(self.grid),(y+0.5)/len(self.grid[1]))),
+                        int((size[0]/len(self.grid)) * 0.4),int(size[0]/len(self.grid[0])*0.1))
+
+
+
+
     def gameInput(self,team,**kwargs):
         
-        if (self.style == 0):
+        if (self.style == CONNECT4_STYLE):
             x = None
             for arg,value in kwargs.items():
                 if (arg == "x"):
@@ -42,8 +83,20 @@ class GameBoard():
                 if (self.grid[x][y+1] != 0):
                     self.grid[x][y] = team
                     return
+        if (self.style == TICTACTOE_STYLE):
+            x = None
+            y = None
+            for arg,value in kwargs.items():
 
-        
+                if (arg == "x"):
+                    x = value
+                if (arg == "y"):
+                    y = value
+
+            if (x == None or y == None):
+                return
+            self.grid[x][y] = team
+            return
 
 
                 
--- main.py
+++ main.py
@@ -4,18 +4,24 @@ import pygame,random,numpy as np,gameboard
 SIZE = np.array((800,880))
 screen = pygame.display.set_mode(SIZE)
 
-connect4 = gameboard.GameBoard(np.array((7,6)),0)
+connect4 = gameboard.GameBoard(np.array((7,6)),gameboard.CONNECT4_STYLE)
 connect4.gameInput(1,x=0)
 connect4.gameInput(2,x=0)
 connect4.gameInput(1,x=1)
 connect4.gameInput(2,x=1)
+
+
+tictactoe = gameboard.GameBoard(np.array((3,3)),gameboard.TICTACTOE_STYLE)
+tictactoe.gameInput(2,x=0,y=0)
+tictactoe.gameInput(1,x=1,y=0)
 
 run = True
 while run:
 
     screen.fill((255,255,255))
 
-    connect4.draw(screen,SIZE/2,SIZE[0]*0.5)
+    connect4.draw(screen,np.array((SIZE[0]/4,SIZE[1]/2)),SIZE[0]*1/3)
+    tictactoe.draw(screen,np.array((SIZE[0]/4 * 3,SIZE[1]/2)),SIZE[0]*1/3)
     #render drawn shapes
     pygame.display.update()
     #render blitted images
